eef7ddc3fd79b6d07a4d56f68fe0d3a092307b66
mikezks <michael.zikes@intauria.com>
2020-01-15 09:35:52 +0100

    Services & Dependency Injection

---
+++ src/app/flight-search/dummy-flight.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { of, Observable } from 'rxjs';
+import { Flight } from '../entities/flight';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DummyFlightService {
+
+  constructor() { }
+
+  find(from: string, to: string): Observable<Flight[]> {
+    return of([
+      {
+        id: 999,
+        from: 'Madrid',
+        to: 'London',
+        date: (new Date()).toISOString(),
+        delayed: false
+      }
+    ]);
+  }
+}
--- src/app/flight-search/flight.service.ts
+++ src/app/flight-search/flight.service.ts
@@ -2,34 +2,15 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { HttpParams, HttpHeaders, HttpClient } from '@angular/common/http';
 import { Flight } from '../entities/flight';
+import { DefaultFlightService } from './default-flight.service';
+import { DummyFlightService } from './dummy-flight.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
+  /* useClass: DefaultFlightService,
+  deps: [HttpClient] */
+  useClass: DummyFlightService
 })
-export class FlightService {
-
-  constructor(private http: HttpClient) { }
-
-  find(from: string, to: string): Observable<Flight[]> {
-    const url = 'http://www.angular.at/api/flight';
-
-    const params = new HttpParams()
-      .set('from', from)
-      .set('to', to);
-
-    const headers = new HttpHeaders()
-      .set('Accept', 'application/json');
-
-    return this.http
-      .get<Flight[]>(url, { params, headers });
-    /* return of([
-      {
-        id: 999,
-        from: 'Madrid',
-        to: 'London',
-        date: (new Date()).toISOString(),
-        delayed: false
-      }
-    ]); */
-  }
+export abstract class FlightService {
+  abstract find(from: string, to: string): Observable<Flight[]>;
 }
