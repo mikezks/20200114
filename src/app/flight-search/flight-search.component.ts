bbbadb4c58eeda137625b74cb37fec075321a840
mikezks <michael.zikes@intauria.com>
2020-01-15 09:13:32 +0100

    RxJS Single Flights

--- src/app/flight-search/flight-search.component.ts
+++ src/app/flight-search/flight-search.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Flight } from '../entities/flight';
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { FlightService } from './flight.service';
+import { Observable, from, zip, interval } from 'rxjs';
+import { switchMap, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-flight-search',
@@ -13,10 +15,23 @@ export class FlightSearchComponent implements OnInit {
   to = 'Graz';
   flights: Flight[] = [];
   selectedFlight: Flight;
+  flight$: Observable<Flight>;
 
   constructor(private flightService: FlightService) { }
 
   ngOnInit(): void {
+    this.flight$ =
+      this.flightService
+        .find(this.from, this.to)
+        .pipe(
+          switchMap(flights =>
+            zip(
+              from(flights),
+              interval(1000)
+            )
+          ),
+          map(([flight, _]) => flight)
+        );
   }
 
   search(): void {
